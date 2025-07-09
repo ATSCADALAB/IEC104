8690051a7a74600dbf38f6eb9f1001074d36ef78
Trần Đức Hải <haitran.qng@outlook.com>
2025-07-09 11:47:36 +0700

    Add project files.

---
+++ Designer/ctlChannelAddress.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IEC104.Designer
+{
+    public partial class ctlChannelAddress : Control
+    {
+        public ctlChannelAddress()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
